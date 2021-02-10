3815015dc3a4c9a7edaac46286b46962593d1bf0
Igor de Paula <principe.borodin@gmail.com>
2021-02-10 18:45:22 -0300

    try

--- processa/Itau_test.go
+++ processa/Itau_test.go
@@ -1,14 +1,13 @@
 package processa
 
 import (
-	Boleto "github.com/IgorDePaula/GOBoleto/structs/Boleto"
-	Date "github.com/IgorDePaula/GOBoleto/structs/Date"
+	"github.com/IgorDePaula/GOBoleto/structs"
 	"testing"
 )
 
 func TestName(t *testing.T) {
-	boleto :=  Boleto{
-		DataVencimento:  Date{Ano:2021, Mes:1, Dia:31},
+	boleto :=  structs.Boleto{
+		DataVencimento:  structs.Date{Ano:2021, Mes:1, Dia:31},
 	}
 	itau := Itau{Boleto: boleto}
 	if itau.FatorVencimento() != 8517{
